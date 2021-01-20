0b351d1bd9d9336a65820c061fd7303a3f5a93fe
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-01-20 12:45:36 +0200

    Started work on AddHotelAsync method for HotelService

---
+++ HotelBookingKata.Tests/HotelServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HotelBookingKata.Tests
+{
+    public class HotelServiceTests
+    {
+        [Fact]
+        public async Task AddHotelAsync_ValidHotelIdAndName_CreatesNewHotel()
+        {
+            var hotelService = new HotelService();
+
+            var hotelId = new HotelId("h4ck");
+            var hotelName = new HotelName("Hacker's Paradise");
+
+            await hotelService.AddHotelAsync(hotelId, hotelName);
+
+
+        }
+    }
+}
---
+++ HotelBookingKata/HotelService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HotelBookingKata
+{
+    public class HotelService
+    {
+        public HotelService()
+        {
+        }
+
+        public async Task AddHotelAsync(HotelId hotelId, HotelName hotelName)
+        {
+            // does nothing for now
+        }
+    }
+}
