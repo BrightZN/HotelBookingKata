8050ebceff56ca8764c75d510fbf55f40a8fb888
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-01-22 12:19:20 +0200

    Implementing setRoom

--- HotelBookingKata.FSharp/Library.fs
+++ HotelBookingKata.FSharp/Library.fs
@@ -3,8 +3,87 @@
 type HotelId = HotelId of string
 type HotelName = HotelName of string
 
+type RoomType = Standard | Junior | Master | Presidential
+type NumberOfRooms = NumberOfRooms of int
+
+type RoomTypeConfig = 
+    { Type: RoomType
+      NumberOfRooms: NumberOfRooms }
+
+module RoomTypeConfig =
+    let updateIfMatch roomType numberOfRooms roomTypeConfig =
+        if roomTypeConfig.Type = roomType then
+            { roomTypeConfig with NumberOfRooms = numberOfRooms }
+        else
+            roomTypeConfig
+
+type Hotel = 
+    { Id: HotelId
+      Name: HotelName
+      RoomTypeConfigs: RoomTypeConfig list }
+
+module Hotel =
+    let addRoom roomType numberOfRooms hotel = 
+        let rooms = hotel.RoomTypeConfigs @ [{ Type = roomType; NumberOfRooms = numberOfRooms}]
+        { hotel with RoomTypeConfigs = rooms }
+
+    let updateRoom roomType numberOfRooms hotel =
+        { hotel with RoomTypeConfigs = hotel.RoomTypeConfigs |> List.map (RoomTypeConfig.updateIfMatch roomType numberOfRooms) }
+
+    let setRoom roomType numberOfRooms hotel =
+        let roomTypeConfig = hotel.RoomTypeConfigs |> List.tryFind (fun r -> r.Type = roomType)
+
+        match roomTypeConfig with
+        | None -> 
+            addRoom roomType numberOfRooms hotel
+        | Some _ ->
+            updateRoom roomType numberOfRooms hotel
+            
+
 module HotelService =
     type AddHotelResult = 
     | HotelAdded
+    | HotelAddingFailed
+    | HotelAlreadyExists
 
-    let addHotel hotelId hotelName = HotelAdded
+    let addHotel doesHotelExist addHotel hotelId hotelName = 
+        async {
+            let! hotelExists = doesHotelExist hotelId
+
+            if hotelExists then
+                return HotelAlreadyExists
+            else
+                let! addResult = addHotel { Id = hotelId; Name = hotelName; RoomTypeConfigs = List.empty }
+
+                return 
+                    match addResult with
+                    | Ok _ -> HotelAdded
+                    | Error _ -> HotelAddingFailed
+        }
+
+    type SetRoomResult =
+    | RoomSet
+    | HotelUpdateFailed
+    | HotelNotFound
+
+    let setRoom getHotelById updateHotel hotelId roomType numberOfRooms = 
+        async {
+            let! hotel = getHotelById hotelId
+            
+            match hotel with
+            | None -> 
+                return HotelNotFound
+            | Some hotel ->
+                let! updateResult = 
+                    hotel 
+                    |> Hotel.setRoom roomType numberOfRooms
+                    |> updateHotel
+
+                match updateResult with
+                | Error _ -> 
+                    return HotelUpdateFailed
+                | Ok _ -> 
+                    return RoomSet
+        }
+
+    
--- HotelBookingKata.FSharpTests/Tests.fs
+++ HotelBookingKata.FSharpTests/Tests.fs
@@ -7,13 +7,76 @@ open Xunit
 module HotelServiceTests =
     open HotelBookingKata.FSharp
 
+    let hotelId = HotelId "h4ck"
+    let hotelName = HotelName "Hacker's Paradise"
+        
+        
+    let doesHotelExist exists _ = 
+        async { 
+            return exists
+        }
+
+    let getHotelById hotels hotelId = 
+        async {
+            return hotels 
+            |> List.tryFind (fun h -> h.Id = hotelId)
+        }
+
     [<Fact>]
-    let ``addHotel - new hotel - creates hotel`` () =
-        let hotelId = HotelId "h4ck"
-        let hotelName = HotelName "Hacker's Paradise"
+    let ``addHotel - new hotel - returns HotelService.HotelAdded`` () =
+        let mutable savedHotel = None
 
-        let result = HotelService.addHotel hotelId hotelName
+        let addHotel hotel =
+            async {
+                savedHotel <- Some hotel
+                return Ok ()
+            }
 
-        let expected = HotelService.HotelAdded
+        async {
 
-        Assert.Equal(expected, result)
+            let! result = HotelService.addHotel (doesHotelExist false) addHotel hotelId hotelName
+
+            let expected = HotelService.HotelAdded
+
+            Assert.Equal(expected, result)
+
+
+            Assert.Equal(Some { Id = hotelId; Name = hotelName; RoomTypeConfigs = List.empty }, savedHotel)
+        }
+
+    [<Fact>]
+    let ``addHotel - existing hotel - returns HotelService.HotelAlreadyExists`` () =
+        let addHotel hotel =
+            async {
+                return Error ()
+            }
+
+        async {
+            let! result = HotelService.addHotel (doesHotelExist true) addHotel hotelId hotelName
+
+            let expected = HotelService.HotelAlreadyExists
+
+            Assert.Equal(expected, result)
+        }
+
+    [<Fact>]
+    let ``setRoom - existing hotel and new room - returns HotelService.RoomSet`` () =
+        let mutable existingHotel = { Id = hotelId; Name = hotelName; RoomTypeConfigs = List.empty }
+
+        let getHotelById = getHotelById [existingHotel]
+
+        let updateHotel hotel =
+            async {
+                existingHotel <- hotel
+                return Ok ()
+            }
+
+        async {
+            let! result = HotelService.setRoom getHotelById updateHotel hotelId RoomType.Standard (NumberOfRooms 5)
+
+            Assert.Equal(HotelService.RoomSet, result)
+
+            Assert.NotEmpty(existingHotel.RoomTypeConfigs)
+            Assert.Contains({ Type = RoomType.Standard; NumberOfRooms = NumberOfRooms 5 }, existingHotel.RoomTypeConfigs)
+        }
+        
